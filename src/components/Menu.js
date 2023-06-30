494c3a51c9a01bd807dbc37e38a291ad905c41d2
felipekmu <132099333+felipekmu@users.noreply.github.com>
2023-06-30 17:18:45 -0300

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Menu(){
     return(
         <nav className="bg-sky-600 text-white p-5 text-base" > 
-            <h1 className="bg-sky-500 text-center font-bold"> Felipe Store </h1>
+            <h1 className="bg-sky-500 text-center font-bold mb-10"> Felipe Store </h1>
             <ul className="flex gap-10 justify-center">
                 <li className="border border-sky-500 p-2 hover:bg-sky-500 transition-all"> <Link href="/"> Início </Link></li>
                 <li className="border border-sky-500 p-2 hover:bg-sky-500 transition-all"> <Link href="/carrinho"> Carrinho </Link></li>
