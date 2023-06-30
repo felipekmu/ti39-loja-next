ce7bb14f0f081289c70056fcf674ee20b83a49cb
felipekmu <132099333+felipekmu@users.noreply.github.com>
2023-06-30 15:49:14 -0300

    Update Rodape.js

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -1,8 +1,8 @@
 export default function Rodape(){
     return(
         <footer className="fixed bottom-0">
-         <hr/>
-         <p> Felipe Store </p>   
+        <hr/>
+         <p> Felipe Store </p>  
         </footer>
     )
 }
